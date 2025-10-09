2910788f75b92b3c3f325c03dbb5e4ff2edf0d97
thomasxwright <wright.ptw@gmail.com>
2025-10-09 14:53:34 -0500

    Got a basic version working that can read and edit longlivedeyes's spreadsheet.

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const { google } = require('googleapis')
+require('dotenv').config({ path: './config/.env' })
+
+const app = express()
+
+app.get("/", async (req, res) => {
+console.log(process.env.PRIVATE_KEY)
+    const auth = new google.auth.GoogleAuth({
+        // keyFile: "credentials.json",
+        credentials: {
+            client_email: process.env.client_email,
+            private_key: process.env.PRIVATE_KEY
+        },
+        scopes: "https://www.googleapis.com/auth/spreadsheets"
+    })
+
+    //create client instance for auth
+    const client = await auth.getClient()
+
+
+    //instance of google sheets api
+    const googleSheets = google.sheets({ version: "v4", auth: client })
+
+    const spreadsheetId = process.env.SPREADSHEET_ID
+
+    //get metadata about spreadsheet
+    const metadata = await googleSheets.spreadsheets.get({
+        auth, spreadsheetId
+    })
+
+    
+    //Write rows to spreadsheet
+    await googleSheets.spreadsheets.values.append({
+        auth,
+        spreadsheetId,
+        range: "Sheet1!A:B",
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [
+                [
+                    new Date().toLocaleTimeString(), "test"
+                ]
+            ]
+        }
+    })
+    
+    //Read rows from spreadsheet
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+        range: "Sheet1!A:A"
+    })
+    
+    res.send(getRows.data)
+})
+
+
+
+app.listen(1337, (req, res) => { console.log('running on 1337') })
